53150904cff15ff8ec4a57557f1928b4d0718900
Мамбетов Азамат <zikbest11@yandex.ru>
2021-08-27 21:17:13 +0300

    Добавил вывод локального адреса в консоль при запуске сервиса.
    - Добавил вывод локального адреса в консоль при запуске сервиса.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import chalk from "chalk";
 import server from "./server";
+import internalIp from "internal-ip";
 
 const HOSTNAME = "0.0.0.0";
 const port: number = Number.parseInt(process.env.PORT ?? "1000", 10);
 
-server.listen(port, HOSTNAME, () => {
+server.listen(port, HOSTNAME, async () => {
 	console.log(
 		chalk.green(
 			`⚡[server]: listening on`,
-			chalk.magenta(`http://localhost:${port}`)
+			chalk.magenta(`http://localhost:${port}`),
+			await internalIp.v4()
 		)
 	);
 });
